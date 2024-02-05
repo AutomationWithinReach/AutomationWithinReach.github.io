153b0d2fe0ab2602f6fbe5e297f9f61444b45930
roachjp4 <92701723+roachjp4@users.noreply.github.com>
2024-02-05 10:32:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 const onProgress = (event) => {
   const progressBar = event.target.querySelector('.progress-bar');
   const updatingBar = event.target.querySelector('.update-bar');
-  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
   if (event.detail.totalProgress === 1) {
     progressBar.classList.add('hide');
     event.target.removeEventListener('progress', onProgress);
